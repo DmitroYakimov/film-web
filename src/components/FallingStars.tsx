9a3473256c723d6a7b6af4047bcc7d5622348dda
DmitroYakimov <dimayakimov18052002@gmail.com>
2025-06-09 19:47:46 +0300

    Ropes

--- src/components/FallingStars.tsx
+++ src/components/FallingStars.tsx
@@ -1,62 +1,16 @@
-import { Box } from '@mui/material';
 import React from 'react';
+import { RopeStar } from './RopeStars';
 
-const stars = [0, 1, 2, 3, 4];
+const starImage = '/star.png';
 
-export default function FallingStars() {
+export const FallingStars = () => {
   return (
-    <Box
-      sx={{
-        position: 'absolute',
-        top: 0,
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        gap: 4,
-        pointerEvents: 'none',
-      }}
-    >
-      {stars.map((_, index) => (
-        <Box
-          key={index}
-          sx={{
-            "@keyframes drop": {
-                    "0%": {
-                        transform: 'translateY(-150px)',
-                        opacity: 0
-                    },
-                    "100%": {
-                        transform: 'translateY(0px)',
-                        opacity: 1
-                    }
-                },
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            animation: 'drop 1.5s ease-out forwards',
-            animationDelay: `${index * 0.1}s`,
-          }}
-        >
-          <Box
-            sx={{
-              width: '2px',
-              height: '150px',
-              backgroundColor: '#333',
-              marginBottom: '8px',
-            }}
-          />
-          <Box
-            sx={{
-              width: 30,
-              height: 30,
-              backgroundColor: 'gold',
-              clipPath: 'polygon(50% 0%, 61% 35%, 98% 35%, \
-                          68% 57%, 79% 91%, 50% 70%, 21% 91%, \
-                          32% 57%, 2% 35%, 39% 35%)',
-            }}
-          />
-        </Box>
-      ))}
-    </Box>
+    <>
+      <RopeStar left="30%" delay={0} starSrc={starImage} />
+      <RopeStar left="40%" delay={0.2} starSrc={starImage} />
+      <RopeStar left="50%" delay={0.4} starSrc={starImage} />
+      <RopeStar left="60%" delay={0.6} starSrc={starImage} />
+      <RopeStar left="70%" delay={0.8} starSrc={starImage} />
+    </>
   );
-}
+};
